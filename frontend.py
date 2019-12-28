ca5c109794409cc09a4420a770566eeacf360127
Unknown <atiyah@post.bgu.ac.il>
2019-12-28 16:07:09 +0200

    ass radio button

--- frontend.py
+++ frontend.py
@@ -1,13 +1,16 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
+from kivy.uix.checkbox import CheckBox
 
-popup = []
+
+popup = {}
 
 class Pop(Widget):
 
@@ -19,8 +22,7 @@ class Pop(Widget):
 
     def closeBtn(self):
         print("h")
-        popup[0].dismiss()
-        # popup.dismiss()
+        popup["p"].dismiss()
 
 
 class MainWindow(Screen):
@@ -28,24 +30,28 @@ class MainWindow(Screen):
     time = ObjectProperty(None)
     num_Recommendations = ObjectProperty(None)
     birth_year = ObjectProperty(None)
-    gender = ObjectProperty(None)
+    # male = ObjectProperty(None)
+    # female = ObjectProperty(None)
+
+    gender = {'M': False, 'F': True}
+
 
     def searchBtn(self):
         self.reset()
-        list =""
-        self.Recommendations(list)
+        list_rec = ""
+        self.recommendations(list_rec)
 
     def reset(self):
         self.current_location.text = ""
         self.time.text = ""
         self.num_Recommendations.text = ""
         self.birth_year.text = ""
-        self.gender.text = ""
+        # self.gender.text = ""
 
     def valid_loc(self,location):
         return True
 
-    def is_valid_input(self,start_loc,time,rec_num,gender,b_year):
+    def is_valid_input(self, start_loc, time, rec_num, gender, b_year):
         valid_loc = self.valid_loc(self, start_loc)
         valid_date = time.isdigit()
         valid_rec = rec_num.isdigit()
@@ -53,11 +59,24 @@ class MainWindow(Screen):
         valid_yaer = b_year.isdigit()
         return valid_date and valid_gender and valid_loc and valid_rec and valid_yaer
 
-    def Recommendations(self,rec):
-        content = Pop("hello")
-        popup.append(Popup(title="title", content = content,size_hint=(0.52,0.8)))
-        popup[0].open()
+    def recommendations(self, rec):
+        if rec is None:
+            content = Pop("pleas try again")
+            title = "bad input"
+        else:
+            content = Pop("hello")
+            title = "Recommendations"
+        popup['p'] = Popup(title=title, content=content, size_hint=(0.52,0.8))
+        popup['p'].open()
         print('hadas')
+
+    def active_female(self):
+        self.gender['F'] = True
+        self.gender['M'] = False
+
+    def active_male(self):
+        self.gender['F'] = False
+        self.gender['M'] = True
 
 class MyApp(App):
     def build(self):
