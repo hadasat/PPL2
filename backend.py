ed3d0a7290cd0f237507c0cb31ab4e7e594a8504
ofirbed <ofirbed@post.bgu.ac.il>
2019-12-31 12:22:36 +0200

    Update backend.py

--- backend.py
+++ backend.py
@@ -5,6 +5,7 @@ import requests
 import sqlite3
 import os
 
+# get the csv and insert it to the db
 current_directory = os.getcwd()
 csv_file_name = "BikeShare.csv"
 csv_path = current_directory + "/" + csv_file_name
@@ -20,7 +21,8 @@ except:
     print("db already exist")
 
 
-
+# get recommandations using webServices.
+# return list of stations. If error returns right message
 def getRecommandations(number_of_recommendations, trip_duration_min, start_station, birth_year, gender):
     c = requests.get('http://127.0.0.1:5000/',params={'number': number_of_recommendations, "time": trip_duration_min,'station': start_station, 'year' : birth_year, 'gender': gender},)
     return c.json()
